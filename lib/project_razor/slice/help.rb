669da8606c2576096451f6a89819b1e4573501cc
DenMat <tu2Bgone@gmail.com>
2012-08-02 09:40:38 -0600

    really adding helper slice

--- lib/project_razor/slice/help.rb
+++ lib/project_razor/slice/help.rb
@@ -1,3 +1,39 @@
 require 'erb'
+require 'yaml'
 
-puts "help here"
+module ProjectRazor
+  module Slice
+    # ProjectRazor Slice Boot
+    # Used for all boot logic by node
+     class Help < ProjectRazor::Slice::Base
+       def initialize(args)
+         @help_args = args
+         @new_slice_style = true
+         @hidden = false
+         @slice_commands = {
+           :default => "what_slice",
+           :else => "what_slice"
+         }
+         @slice_name = "Help"
+         @engine = ProjectRazor::Engine.instance
+       end
+
+       def get_help(slice,action)
+         help_details = YAML.load_file(File.join(File.dirname(__FILE__), "#{slice.downcase}/#{slice}.yaml"))
+         help_template = ERB.new(File.open(File.join(File.dirname(__FILE__), "#{slice.downcase}/#{slice}.erb"), 'r'), nil, "%")
+         summary = help_details['command'][0][action][0]['summary']
+         help_text = help_details['command'][0][action][1]['subactions'][0]['all']
+         this_action = action
+         
+         puts help_template.result(binding)
+       end
+
+       def what_slice()
+         what_slice = @help_args.shift
+         what_action = @help_args.shift
+         get_help(what_slice, what_action)
+       end
+         
+     end
+  end
+end
